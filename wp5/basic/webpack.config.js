5475bee930b94004723414657b6f6d91ff52fd3d
lipeng123461 <247934357lip@gmail.com>
2021-02-01 13:47:11 +0800

    修改gitignore增加基本文件

---
+++ wp5/basic/webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+module.exports = {
+    context: process.cwd(), // http://nodejs.cn/api/process/process_cwd.html
+    entry: './src/index.js',
+    // mode: 'development', // 
+    output: {
+        path: path.resolve(__dirname, 'dist'), // __dirname 表示文件的绝对路径,一个全局变量
+        filename: 'bundle.js',
+        // publicPath: 'assets' // 配置打包后的目录前缀 最终文件名为：publicPath+filename
+    },
+    module: {
+        rules:[
+            {test:/\.txt$/, use:'raw-loader'}
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template:'./src/index.html'
+        })
+    ],
+    devServer: {
+        contentBase: path.resolve(__dirname, 'static'), // 当前目录下的文件夹作为公共资源使用
+        writeToDisk:true, // 打包后的文件写到硬盘上
+        compress:true, // 是否启动压缩
+        port:8080, // 指定HTTP服务器的端口号
+        open:true, // 是否自动打开浏览器
+        // publicPath: '/assets' // 指定浏览器访问的地址后缀： 一般是 http://localhost:8080  加上参数之后访问的地址会变成http://localhost:8080/assets
+    }
+}
+
+// resolve  会把相对路径转换成绝对路径，  join只是连接   参考nodeapi，或者webpack自定的函数
+// mode：支持各种模式
+// publicPath详解 https://juejin.cn/post/6844903601060446221
